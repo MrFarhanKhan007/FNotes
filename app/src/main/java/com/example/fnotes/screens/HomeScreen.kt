9a8177eac29dea3df8a368be28309926d377bd4a
Farhan Khan <fk95034@gmail.com>
2024-01-22 23:56:36 +0530

    Finished Basic HomeScreen.kt layout

--- app/src/main/java/com/example/fnotes/screens/HomeScreen.kt
+++ app/src/main/java/com/example/fnotes/screens/HomeScreen.kt
@@ -1,17 +1,66 @@
 package com.example.fnotes.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.fnotes.R
+import com.example.fnotes.ui.theme.textColor
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-
+    MyApp(navController = navController)
 }
 
-@Preview
+@Composable
+fun MyApp(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier.height(50.dp))
+        Text(
+            text = stringResource(R.string.tasks),
+            modifier.align(Alignment.CenterHorizontally),
+            style = TextStyle(
+                color = textColor,
+                textAlign = TextAlign.Center,
+                fontSize = 50.sp
+            )
+        )
+        Spacer(modifier.height(200.dp))
+        NoAvailableTaskText()
+    }
+}
+
+@Composable
+fun NoAvailableTaskText(modifier: Modifier=Modifier){
+    Text(
+        text = stringResource(R.string.no_available_tasks_right_now_click_the_icon_to_add_a_task),
+        style = TextStyle(
+            color = textColor,
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center
+        )
+    )
+}
+@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(navController= rememberNavController())
+    HomeScreen(navController = rememberNavController())
+
 }
--- app/src/main/java/com/example/fnotes/ui/theme/Theme.kt
+++ app/src/main/java/com/example/fnotes/ui/theme/Theme.kt
@@ -18,13 +18,15 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = backgroundColor
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = backgroundColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -57,7 +59,8 @@ fun FNotesTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = textColor.toArgb()
+            window.navigationBarColor= textColor.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
